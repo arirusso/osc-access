dd642e6af61dad8d118dd49a598633df0b7a51c0
Ari Russo <ari.russo+github.com@gmail.com>
2011-10-12 11:58:34 -0300

    Edited examples/map_method.rb via GitHub

--- examples/map_method.rb
+++ examples/map_method.rb
@@ -9,7 +9,7 @@ class Instrument
 
   attr_accessor :pitch, :velocity
       
-  osc_writer :pitch, "/1/fader1", :range => { :input => 0..1, :output => 0..127 }
+  attr_osc :pitch, "/1/fader1", :range => { :input => 0..1, :output => 0..127 }
   
   osc_map("/2/fader2", :from => 0..1, :to => 0..127) do |subject, value|
     p value
