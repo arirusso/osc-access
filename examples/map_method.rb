c1af8a091d71f754bdc7e08d86b04efde0ff8da7
Ari Russo <ari.russo+github.com@gmail.com>
2011-10-12 13:39:54 -0300

    Edited examples/map_method.rb via GitHub

--- examples/map_method.rb
+++ examples/map_method.rb
@@ -9,6 +9,8 @@ class Instrument
       
   attr_osc :pitch, :pattern => "/1/fader1", :range => { :input => 0..1, :output => 0..127 }
   
+  attr_osc :pulse_width, :pattern => "/2/fader2"
+  
   attr_osc :velocity, :pattern => "/2/fader2", :range => 0..127
   
 end
