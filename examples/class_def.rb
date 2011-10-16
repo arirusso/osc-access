a4a21ec5ec7ddf240aa0021dd1a3a7f779bb1eca
Ari Russo <ari.russo@gmail.com>
2011-10-16 19:56:25 -0400

    cleanup

--- examples/class_def.rb
+++ examples/class_def.rb
@@ -6,15 +6,17 @@ require "osc-access"
 class Instrument
   
   include OSCAccessible
+
+  osc_receive "/1/fader1" do |instance, msg|
+    val = instance.osc_translate(msg.args.first, :local => 0..127, :remote => 0..1 )
+    instance.velocity = val
+  end
   
-  osc_accessor :pitch, 
-               :pattern => "/1/fader1", 
-               :range => { :remote => 0..1, :local => 0..127 }
-  #osc_writer :pulse_width, :pattern => "/1/fader2"
-  
-  #attr_osc :velocity, :pattern => "/1/rotary1", :range => 0..127
+  def velocity=(val)
+    puts "fader1 has value #{val}"
+  end
   
 end
 
 i = Instrument.new
-i.osc_start.join
+i.osc_start(:input_port => 8000).join
