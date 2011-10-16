6bdd3ac3b982bbcb45fe8f52f3bcb5c037f319ea
Ari Russo <ari.russo@gmail.com>
2011-10-15 20:41:14 -0400

    decoupling

--- lib/osc-object/io.rb
+++ lib/osc-object/io.rb
@@ -8,22 +8,9 @@ module OSCObject
     attr_reader :thread
     def_delegators :thread, :join, :exit
     
-    def initialize(target_obj, scheme, options = {})
-
-      ports = scheme.ports
-      port = ports[:receive] || options[:receive_port] || DefaultReceivePort
-      map = options[:map]
-
-      # pull osc server info from class instance vars
-      @server = scheme.server || OSC::EMServer.new(port)
-
-      send_ip = options[:send_ip] || scheme.send_ip
-
-      unless send_ip.nil?
-        remote_port = ports[:send] || options[:send_port] || DefaultSendPort
-        @client = OSC::Client.new( options[:send_ip], remote_port )
-      end
-      
+    def initialize(target_obj, port_spec, options = {})
+      @server = self.class.server(port_spec.receive)
+      @client = self.class.client(options[:send_ip], ports_spec.transmit) unless options[:send_ip].nil?
     end
     
     def transmit(*a)
@@ -39,6 +26,19 @@ module OSCObject
         Thread.abort_on_exception = true
         @server.run
       end
+    end
+    
+    def self.server(port)
+      @servers ||= {}
+      @servers[port] ||= OSC::EMServer.new(port) 
+      @servers[port]
+    end
+    
+    def self.client(ip, port)
+      @clients ||= {}
+      @clients[ip] ||= {}
+      @clients[ip][port] ||= OSC::Client.new(ip, port)
+      @clients[ip][port]
     end
 
   end
