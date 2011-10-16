a4ed8bae2f7afb0ae10b2f0710f9847afb07b42f
Ari Russo <ari.russo@gmail.com>
2011-10-15 20:40:46 -0400

    port spec object

---
+++ lib/osc-object/user_port_spec.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+module OSCObject
+  
+  class UserPortSpec
+    
+    attr_reader :receive, :transmit
+    
+    def initialize(spec)
+      unless spec.nil? 
+        case spec
+          when Numeric then @receive = spec
+          when Hash then
+            @receive = spec[:receive]
+            @transmit = spec[:send] || spec[:transmit]
+        end
+      end      
+    end
+
+  end
+
+end
