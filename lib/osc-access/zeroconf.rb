76ad47ae2ed73833c1349d52bbac30deadbae05c
Ari Russo <ari.russo@gmail.com>
2011-10-24 22:01:21 -0400

    gettin started...

---
+++ lib/osc-access/zeroconf.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+#
+# Zeroconf support
+#
+# http://www.zeroconf.org
+#
+#
+
+#
+# much of this code was lifted from Zosc
+# http://github.com/samBiotic/ruby-zosc
+# Copyright (c) 2011 Sam Birkhead and released under the MIT License
+#
+#
+
+require "dnssd"
+
+module OSCAccess
+
+  class Zeroconf
+        
+  end
+
+end
