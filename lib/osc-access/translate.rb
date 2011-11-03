b8804bd09c5badcac1674108a05a1a2ac01f7ac0
Ari Russo <ari.russo@gmail.com>
2011-11-03 00:18:19 -0400

    translate improvements

--- lib/osc-access/translate.rb
+++ lib/osc-access/translate.rb
@@ -1,64 +1,109 @@
 #!/usr/bin/env ruby
 module OSCAccess
-  
-  module Analog
 
+  module Analog
+    
     def self.new(from, to, options = {})
       case to
-        when Array then Set.new(from, to, options)
-        when ::Range then Range.new(from, to, options)
+        when Array then Output::Set.new(Input.new(from), to, options)
+        when ::Range then Output::Range.new(Input.new(from), to, options)
       end
     end
     
-    class Range
+    module Input
       
-      def initialize(from_range, to_range, options = {})
-        @type = options[:type]
-
-        @from_range = from_range
-        @to_range = to_range
+      def self.new(input)
+        case input
+          when Array then Input::Set.new(input)
+          when ::Range then Input::Range.new(input)
+        end
       end
-
-      def process(input, options = {})
-        to_range_len = (@to_range.last - @to_range.first).abs
-        from_range_len = (@from_range.last - @from_range.first).abs
-        abs_input = input - @from_range.first
-        proportion = to_range_len.to_f / from_range_len.to_f
-        abs_output = proportion.to_f * abs_input.to_f
-        output = abs_output + @to_range.first
-
-        type = options[:type] || @type
-        float_requested = !type.nil? && type.to_s.downcase == "float"
-        float_requested ? output : output.to_i
+      
+      class Range
+        
+        def initialize(range)
+          @range = range
+        end
+        
+        def numerator(input)
+          (input - @range.first).to_f
+        end
+        
+        def denominator
+          (@range.last - @range.first).abs.to_f
+        end
+        
       end
+      
+      class Set
 
+        def initialize(set)
+          @set = set
+        end
+        
+        def numerator(input)
+          @set.index(input).to_f
+        end
+        
+        def denominator
+          (@set.size - 1).to_f
+        end
+                
+      end
+      
     end
 
-    class Set
+    module Output
       
-      def initialize(from_range, to_set, options = {})
-        @from_range = from_range
-        @to_set = to_set
+      class Range
+        
+        def initialize(from, to_range, options = {})
+          @type = options[:type]
+
+          @from = from
+          @to_range = to_range
+        end
+
+        def process(input, options = {})
+          to_range_len = (@to_range.last - @to_range.first).abs
+          
+          proportion = to_range_len.to_f / @from.denominator
+          abs_output = proportion.to_f * @from.numerator(input)
+          output = abs_output + @to_range.first
+
+          type = options[:type] || @type
+          float_requested = !type.nil? && type.to_s.downcase == "float"
+          float_requested ? output : output.to_i
+        end
+
       end
 
-      def process(input, options = {})
-        from_range_length = @from_range.last - @from_range.first
-        input_pct = input.to_f / from_range_length.to_f
-        index = ((input_pct * @to_set.size).to_i - 1)
-        @to_set.at(index)
+      class Set
+        
+        def initialize(from, to_set, options = {})
+          @from = from
+          @to_set = to_set
+        end
+
+        def process(input, options = {})
+          proportion = @from.numerator(input) / @from.denominator
+          index = [((proportion * @to_set.size).to_i - 1), 0].max
+          @to_set.at(index)
+        end
+
       end
 
     end
 
   end
-  
+
   class Translate
     
     def self.using(value, range, options = {})
       to_local = options[:to_local].nil? ? true : options[:to_local]
       new_vals = [value].flatten.compact.map do |single_val|
         if range == :boolean
-          to_local ? (single_val <= 0 ? false : true) : (single_val ? 1 : 0)
+        to_local ? (single_val <= 0 ? false : true) : (single_val ? 1 : 0)
         else
           if range.kind_of?(Range) || range.kind_of?(Array)
             remote = DefaultRemoteRange
--- test/test_translate.rb
+++ test/test_translate.rb
@@ -8,53 +8,79 @@ class TranslateTest < Test::Unit::TestCase
   include TestHelper
   
   def test_range_process_amplify
-    ra = Analog::Range.new(0..1, 0..127)
+    ra = Analog.new(0..1, 0..127)
     output = ra.process(0.10)
     assert_equal(12, output)
   end
   
   def test_range_process_deamplify
-    ra = Analog::Range.new(0..150, 0..15)
+    ra = Analog.new(0..150, 0..15)
     output = ra.process(22)
     assert_equal(2, output)    
   end
   
-  def test_negative_range
-    
+  def test_neg_posi_to_posi
+    ra = Analog.new(-24..24, 0..3, :type => :float)
+    output = ra.process(10)
+    assert_equal(2.125, output)      
   end
   
-  #def test_from_set_to_range
-  #  ra = Analog::Range.new([0, 2, 4, 8, 16, 64], 0..10)
-  #  output = ra.process(8)
-  #  assert_equal(6, output)    
-  #end
+  def test_neg_posi_to_neg_posi
+    ra = Analog.new(-24..24, -3..3, :type => :float)
+    output = ra.process(10)
+    assert_equal(1.25, output)      
+  end
+  
+  def test_neg_to_neg_posi
+    ra = Analog.new(-24..-12, -3..3, :type => :float)
+    output = ra.process(-14)
+    assert_equal(2, output)      
+  end
+  
+  def test_neg_to_neg
+    ra = Analog.new(-24..-12, -5..-3, :type => :float)
+    output = ra.process(-16)
+    assert_equal(-3.666666666666667, output)      
+  end
+  
+  def test_neg_to_posi
+    ra = Analog.new(-24..-12, 1..3, :type => :float)
+    output = ra.process(-18)
+    assert_equal(2, output)      
+  end
+  
+  def test_from_set_to_range
+    ra = Analog.new([0, 2, 4, 8, 16, 64], 0..10)
+    output = ra.process(8)
+    assert_equal(6, output)    
+  end
   
   def test_range_process_float
-    ra = Analog::Range.new(0..120, 0..1, :type => :float)
+    ra = Analog.new(0..120, 0..1, :type => :float)
     output = ra.process(12)
     assert_equal(0.10, output)     
   end
   
   def test_set_ascending
-    sa = Analog::Set.new(0..1, [0, 2, 4, 8, 12, 16, 32, 64, 128, 512])
+    sa = Analog.new(0..1, [0, 2, 4, 8, 12, 16, 32, 64, 128, 512])
     output = sa.process(0.40)
     assert_equal(8, output)
   end
   
   def test_set_descending
-    sa = Analog::Set.new(0..1, [512, 128, 64, 32, 16, 12, 8, 4, 2, 0])
+    sa = Analog.new(0..1, [512, 128, 64, 32, 16, 12, 8, 4, 2, 0])
     output = sa.process(0.40)
     assert_equal(32, output)
   end
   
   def test_range_class
     a = Analog.new(0..1, 0..100)
-    assert_equal(Analog::Range, a.class)
+    assert_equal(Analog::Output::Range, a.class)
   end
   
   def test_set_class
     a = Analog.new(0..1, [0, 10, 20, 50])
-    assert_equal(Analog::Set, a.class)
+    assert_equal(Analog::Output::Set, a.class)
   end
   
 end
